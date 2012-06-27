a9175a8fb499d6759614865ae2e4d43373f17b38
Paul Madden <maddenp@colorado.edu>
2012-06-26 19:59:22 -0600

    Update master

--- src/bluebook.java
+++ src/bluebook.java
@@ -1,4 +1,4 @@
-// Copyright 2012 Paul Madden
+// Copyright 2012 Paul Madden (maddenp@colorado.edu)
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
